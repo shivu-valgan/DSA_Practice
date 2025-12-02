3c1fa243f92a4d8419d978afcc88a7ed127e146f
shivu <techshivu77@hotmail.com>
2025-12-03 00:46:50 +0530

    quickSort

--- src/Sorting/AdvancedLevel/Quick.java
+++ src/Sorting/AdvancedLevel/Quick.java
@@ -1,4 +1,42 @@
 package Sorting.AdvancedLevel;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Quick {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) arr[i]=in.nextInt();
+        quickSort(arr,0,n-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void quickSort(int[] arr, int low, int high) {
+        if(low<high){
+            int pi = partition(arr,low,high);
+            quickSort(arr,low,pi-1);
+            quickSort(arr,pi+1,high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i=low-1;
+        for(int j=low; j<=high-1; j++){
+            if(arr[j]<pivot){
+                i++;
+                swap(arr,i,j);
+            }
+        }
+        swap(arr,i+1,high);
+        return i+1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
